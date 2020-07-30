a90799112f3131bd2f68cb9b522dab25aa395c9f
Victor Avila <avilavictor1570@gmail.com>
2020-07-30 03:18:13 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,11 @@
-
+class CashRegister
+  
+  attr_accessor :total, :discount, :last_transaction, :items
+  
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+  
+end
