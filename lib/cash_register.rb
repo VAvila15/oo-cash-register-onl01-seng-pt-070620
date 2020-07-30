0f6f60a0c2c8470c133bc3af606ab24e8aec16ed
Victor Avila <avilavictor1570@gmail.com>
2020-07-30 04:07:46 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,7 +12,7 @@ class CashRegister
 
   def add_item(title,price,quantity = 1)
     quantity.times do 
-      @items == title
+      @items << title
     end
     @last_total = @total
     @total = price * quantity
