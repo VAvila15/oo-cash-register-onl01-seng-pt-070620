62b1a894c8487a5d41d214fba7a9c5a1e42f66c3
Victor Avila <avilavictor1570@gmail.com>
2020-07-30 03:32:24 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
   def add_item(title,price,quantity = 1)
     if quantity > 1
       i = 0
-    while i<quantity
+    while i < quantity
       @items << title
       i+=1
     end
@@ -20,7 +20,6 @@ class CashRegister
     end
     @total += price*quantity
     @last_transaction_amount = @total
-    @total
   end
   
   def apply_discount()
